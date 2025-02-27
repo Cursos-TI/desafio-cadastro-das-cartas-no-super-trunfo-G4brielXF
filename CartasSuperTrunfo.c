15bcc4af86c1d8aebb035de87eb4cfceae035813
G4brielXF <xavierdefreitasgabriel@gmail.com>
2025-02-27 19:16:49 +0000

    Usei operadores matemáticos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,18 @@
 int main() {
 
     /*CARTA1:*/                       /*Não consegui colocar "." nas casas das centenas*/
+
+    printf ("***BEM VINDO AO SISTEMA DE CADASTRO DE CARTAS SUPER TRUNFO*** \n");
+
     char Estado1 = 'A';
-    char Nome1[50] = "minas gerais"; 
-    char Código1[20] = "A01";
+    char Nome1[15] = "minasgerais"; 
     int População1 = 21320000;
-    float Área1 = 58651398;
+    char Código1[15] = "A01";
+    float Área1 = 586.520;
     float PIB1 = 10200000000000;
     int Pontos1 = 30; 
+    float DensidadePopulacional1 = (float) População1 / Área1;
+    float PIBpercapta1 = (float) PIB1 / População1;
 
     /*Carta 1: local para cadastrar as informações*/
     printf ("Digite a letra do Estado 1: \n");
@@ -21,11 +26,11 @@ int main() {
     printf ("Digite o nome da cidade 1: \n");
     scanf (" %s", Nome1);
 
-    printf ("Digite o código da carta da cidade 1: \n");
-    scanf (" %s", Código1);
-
     printf ("Digite quantos habitantes há na cidade 1: \n");
     scanf (" %d", &População1);
+
+    printf ("Digite o código da carta da cidade 1: \n");
+    scanf (" %s", Código1);
 
     printf ("Digite a área da cidade 1 (em km²): \n");
     scanf (" %f", &Área1);
@@ -36,7 +41,30 @@ int main() {
     printf ("Digite quantos pontos turísticos há na cidade 1: \n");
     scanf (" %d", &Pontos1);
 
+    //Calculo para a Densidade Populacional
+
+    printf ("**VAMOS CALCULAR A DENSIDADE POPULACIONAL DA CIDADE 1** \n");
+
+    printf ("Entre com o número da população 1: \n");
+    scanf ("%d", &População1);
+    printf ("Entre com a área da cidade 1: \n");
+    scanf ("%f", &Área1);
+
+    //Calculo para o PIB per capita
+
+    printf ("**VAMOS CALCULAR O PIB PER CAPITA DA CIDADE 1** \n");
+
+    printf ("Entre com o PIB da cidade 1: \n");
+    scanf ("%d", &PIB1);
+    printf ("Entre com o número da população 1: \n");
+    scanf ("%f", &População1);
+
+
+
     /*Carta 1: Resultados do cadastro*/
+
+    printf ("****RESULTADOS DA CIDADE 1**** \n");
+
     printf ("Letra da cidade 1:  %c \n", Estado1);
     printf ("Nome da cidade 1:  %s \n", Nome1);
     printf ("Código da carta 1:  %s \n", Código1);
@@ -44,15 +72,22 @@ int main() {
     printf ("Área 1:  %f \n", Área1);
     printf ("PIB da cidade 1:  %f \n", PIB1);
     printf ("Pontos turísticos da cidade 1:  %d \n", Pontos1);
+    printf ("A Densidade Populacional 1 é: %4.f \n", DensidadePopulacional1);
+    printf ("A Renda per capita 1 é: %4.f \n", PIBpercapta1);
+
+
+
 
     /*Carta 2:*/
     char Estado2 = 'B';
-    char Nome2 [50] = "Ceará";
-    char Código2 [20] = "B02";
+    char Nome2 [15] = "Ceará";
     int População2 = 8795000;
-    float Área2 = 148886;
+    char Código2 [15] = "B02";
+    float Área2 = 148.886;
     float PIB2 = 194885;
     int Pontos2 = 19;
+    float DensidadePopulacional2 = (float) População2 / Área2;
+    float PIBpercapta2 = (float) PIB2 / População2;
 
     /*Carta 2: local para cadastrar as informações*/
     printf ("Digite a letra do Estado 2: \n");
@@ -61,11 +96,11 @@ int main() {
     printf ("Digite o Nome da cidade 2: \n");
     scanf (" %s", &Nome2);
 
-    printf ("Digite o Código da carta 2: \n");
-    scanf (" %s", &Código2);
-
     printf ("Digite quantos habitantes há na cidade 2: \n");
     scanf (" %d", &População2);
+
+    printf ("Digite o Código da carta 2: \n");
+    scanf (" %s", &Código2);
 
     printf ("Digite qual a área da cidade 2 (em km²): \n");
     scanf (" %f", &Área2);
@@ -76,7 +111,27 @@ int main() {
     printf ("Digite quantos pontos turísticos há na cidade 2: \n");
     scanf (" %d", &Pontos2);
 
+    //Calculo para a Densidade Populacional
+
+    printf ("**VAMOS CALCULAR A DENSIDADE POPULACIONAL DA CIDADE 2** \n");
+
+    printf ("Entre com o número da população 2: \n");
+    scanf ("%d", &População2);
+    printf ("Entre com a área da cidade 2: \n");
+    scanf ("%f", &Área2);
+
+    //Calculo para o PIB per capita
+
+    printf ("**VAMOS CALCULAR O PIB PER CAPITA DA CIDADE 2** \n");
+
+    printf ("Entre com PIB da cidade 2: \n");
+    scanf ("%d", &PIB2);
+    printf ("Entre com o número da população 2: \n");
+    scanf ("%f", &População2);
+
     /*Carta 2: Resultados do cadastro*/
+
+    printf ("****RESULTADOS DA CIDADE 2**** \n");
 
     printf ("Letra da cidade 2:  %c \n", Estado2);
     printf ("Nome da cidade 2:  %s \n", Nome2);
@@ -85,6 +140,9 @@ int main() {
     printf ("Área da cidade 2:  %f \n", Área2);
     printf ("PIB da cidade 2:  %f \n", PIB2);
     printf ("Pontos turísticos da cidade 2:  %d \n", Pontos2);
+    printf ("A Densidade Populacional 2 é: %4.f \n", DensidadePopulacional2);
+    printf ("A Renda per capita 2 é: %4.f \n", PIBpercapta2);
+
 
     return 0;
 }
